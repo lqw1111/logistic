88a1bac4c66da7f65177e4715d847d0a6e4dd6ac
lqw1111 <369487836luo@gmail.com>
2020-10-15 15:18:14 -0400

    fix the calculate isue

--- src/main/java/com/logistic/project/controller/PriceCalculateController.java
+++ src/main/java/com/logistic/project/controller/PriceCalculateController.java
@@ -4,6 +4,8 @@ import com.logistic.project.dto.PriceCalculateRequestDTO;
 import com.logistic.project.service.PriceCalculateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -17,8 +19,9 @@ public class PriceCalculateController {
     @RequestMapping(value = "", method = RequestMethod.POST)
     public List<Map<String, Object>> calculate(@RequestBody PriceCalculateRequestDTO priceCalculateRequestDTO) throws Exception{
         return priceCalculateService.calculate(priceCalculateRequestDTO.getCountryId(),
-                priceCalculateRequestDTO.getLength(), priceCalculateRequestDTO.getHight(),
-                priceCalculateRequestDTO.getWidth(),
+                priceCalculateRequestDTO.getLength() == null ? new BigDecimal(0.1) : priceCalculateRequestDTO.getLength(),
+                priceCalculateRequestDTO.getHight() == null ? new BigDecimal(0.1) : priceCalculateRequestDTO.getHight(),
+                priceCalculateRequestDTO.getWidth() == null ? new BigDecimal(0.1) : priceCalculateRequestDTO.getWidth(),
                 priceCalculateRequestDTO.getWeight());
     }
 }

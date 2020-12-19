8850dba96e4b16b88716892b552b715306dc7831
lqw1111 <369487836luo@gmail.com>
2020-12-19 11:54:36 -0500

    add new payment method

--- src/main/java/com/logistic/project/enumeration/PaymentType.java
+++ src/main/java/com/logistic/project/enumeration/PaymentType.java
@@ -3,7 +3,8 @@ package com.logistic.project.enumeration;
 public enum PaymentType {
 
     WECHAT(1, "wechat"),
-    ALIPAY(2, "alipay");
+    ALIPAY(2, "alipay"),
+    INTERACT_ETRANSFER(3, "interact-etransfer");
 
     private Integer id;
     private String name;
